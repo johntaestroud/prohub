5a59891a37d7522612be761435e20ddd0aaf86c2
unknown <johntae.stroud@gmail.com>
2018-08-16 10:35:43 -0400

    Displaying error msgs

--- register.php
+++ register.php
@@ -18,7 +18,7 @@ $em2 = "";
 $password = "";
 $password2 = "";
 $date = "";
-$error_array = ""; #holds error msgs
+$error_array = array(); #array() - Declaring into an empty array. Holds error msgs
 
 #$_POST - used to collect values from a form with method="post"
 if(isset($_POST['register_button'])){ #isset - determines if a var is set and is not a null
@@ -57,50 +57,51 @@ if(isset($_POST['register_button'])){ #isset - determines if a var is set and is
   if($em == $em2) { #check for equality
       #filter_var - Filters a variable with a specified filter. Check if email is in valid format
       if(filter_var($em, FILTER_VALIDATE_EMAIL)) {
-        #check if em equals the validated version of the email
-        $em = filter_var($em, FILTER_VALIDATE_EMAIL);
+          #check if em equals the validated version of the email
+          $em = filter_var($em, FILTER_VALIDATE_EMAIL);
 
-        #Check if email already exist
-        $e_check = mysqli_query($con, "SELECT email FROM users WHERE email='$em'");
+          #Check if email already exist
+          $e_check = mysqli_query($con, "SELECT email FROM users WHERE email='$em'");
 
-        #mysqli_num_rows - returns the number of rows in a result set. Count the number of rows return
-        $num_rows = mysqli_num_rows($e_check);
+          #mysqli_num_rows - returns the number of rows in a result set. Count the number of rows return
+          $num_rows = mysqli_num_rows($e_check);
+          #array_push - Push one or more elements onto the end of array. When an err occurs it gets push into the array
+          if($num_rows > 0) {
+            array_push($error_array, "Email already in use<br>");
+          }
 
-        if($num_rows > 0) {
-          echo "Email already in use";
+        }
+        else {
+           array_push($error_array, "Invalid email format<br>");
         }
 
       }
       else {
-        echo "Invalid format";
+        // echo "Emails don't match<br>";
+        array_push($error_array, "Emails don't match<br>");
       }
 
       if(strlen($fname) > 25 || strlen($fname) < 2) {
-        echo "Your first name must be between 2 and 25 characters";
+        array_push($error_array, "Your first name must be between 2 and 25 characters<br>");
       }
 
       if(strlen($lname) > 25 || strlen($lname) < 2) {
-        echo "Your last name must be between 2 and 25 characters";
+        array_push($error_array, "Your last name must be between 2 and 25 characters<br>");
       }
 
       if($password != $password2) {
-        echo "Your passwords do not match";
+        array_push($error_array, "Your passwords do not match<br>");
       }
       else {
         #preg_match - Performs a regular expression match, checking to see if val only contains characters and numbers
         if(preg_match('/[^A-Za-z0-9]/', $password)) {
-          echo "Your password can only contain characters or numbers";
+          array_push($error_array, "Your password can only contain characters or numbers<br>");
         }
       }
       #strlen - Getting string length
       if(strlen($password > 30 || strlen($password) < 5)) {
-        echo "Your password must be between 5 and 30 characters";
+        array_push($error_array, "Your password must be between 5 and 30 characters<br>");
       }
-
-  }
-  else {
-      echo "Emails don't match";
-  }
 
 }
 
@@ -120,32 +121,49 @@ if(isset($_POST['register_button'])){ #isset - determines if a var is set and is
     <form action="register.php" method="post"> <!---->
       <input type="text" name="reg_fname" placeholder="First Name" value="<?php
       if(isset($_SESSION['reg_fname'])) { #saving the input value
-        echo $_SESSION['reg_fname'];
+          echo $_SESSION['reg_fname'];
       }
       ?>" required>
       <br>
+      <?php if(in_array("Your first name must be between 2 and 25 characters<br>", $error_array)) echo "Your first name must be between 2 and 25 characters<br>"; ?>
+
       <input type="text" name="reg_lname" placeholder="Last Name" value="<?php
       if(isset($_SESSION['reg_lname'])) { #saving the input value
-        echo $_SESSION['reg_lname'];
+          echo $_SESSION['reg_lname'];
       }
       ?>" required>
       <br>
+      <?php if(in_array("Your last name must be between 2 and 25 characters<br>", $error_array)) echo "Your last name must be between 2 and 25 characters<br>"; ?>
+
       <input type="email" name="reg_email" placeholder="Email" value="<?php
       if(isset($_SESSION['reg_email'])) { #saving the input value
-        echo $_SESSION['reg_email'];
+          echo $_SESSION['reg_email'];
       }
       ?>" required>
       <br>
+
       <input type="email" name="reg_email2" placeholder="Confirm Email" value="<?php
       if(isset($_SESSION['reg_email2'])) { #saving the input value
-        echo $_SESSION['reg_email2'];
+          echo $_SESSION['reg_email2'];
       }
       ?>" required>
       <br>
+
+      <!-- in_array - checks if a value exists in an array. If the needle is in the haystack -->
+      <?php if(in_array("Email already in use<br>", $error_array)) echo "Email already in use<br>";
+       else if(in_array("Invalid email format<br>", $error_array)) echo "Invalid email format<br>";
+       else if(in_array("Emails don't match<br>", $error_array)) echo "Emails don't match<br>"; ?>
+
       <input type="password" name="reg_password" placeholder="Password" required>
       <br>
       <input type="password" name="reg_password2" placeholder="Confirm Password" required>
       <br>
+
+      <!-- in_array - checks if a value exists in an array. If the needle is in the haystack -->
+      <?php if(in_array("Your passwords do not match<br>", $error_array)) echo "Your passwords do not match<br>";
+       else if(in_array("Your password can only contain characters or numbers<br", $error_array)) echo "Your password can only contain characters or numbers<br";
+       else if(in_array("Your password must be between 5 and 30 characters<br>>", $error_array)) echo "Your password must be between 5 and 30 characters<br>"; ?>
+
       <input type="submit" name="register_button" value="Register">
 
     </form>
